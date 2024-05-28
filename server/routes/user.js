ed21be31f7328618d9e6dcefb621b16844ac25e8
sneha931 <cs22157@velammalitech.edu.in>
2024-05-28 21:12:59 +0530

    secondcommit

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
-router.use(cors())
 const router = express.Router();
 const UserModel = require('../models/mongo');
 const nodemailer=require('nodemailer')
