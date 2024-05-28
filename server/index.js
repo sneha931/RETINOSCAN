700d239e1ecc095e339b1e6307cd1ed371d39417
sneha931 <cs22157@velammalitech.edu.in>
2024-05-28 21:42:37 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -3,14 +3,11 @@ const dotenv = require('dotenv');
 const mongoose = require("mongoose");
 dotenv.config();
 const cors = require("cors");
-const app = express();
-
 app.use(cors({
     origin: "https://retinoscan-client.vercel.app",
-    methods: ['GET', 'POST','OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
