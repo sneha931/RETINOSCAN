6a2054930a96095201b8d90535756551a7805562
sneha931 <cs22157@velammalitech.edu.in>
2024-05-29 23:06:02 +0530

    commits

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"https://665762e131072b057e6e057d--adorable-buttercream-1989b9.netlify.app/",
+    credentials:true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
